a4fa7508ac1d8fb25771d24e430f89c79930640b
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-09-18 22:02:20 -0300

    feat(project): estrutura inicial

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    // Configure your E2E tests here
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    baseUrl: "https://notes-serverless-app.com",
+    defaultCommandTimeout: 10000,
+    watchForFileChanges: false,
+  },
+});
