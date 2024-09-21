b17b40e6a9aa01597d95579bbe3b2783a4c04585
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-09-21 13:25:51 -0300

    fix(plugins): ajuste no import do @cypress-grep

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,8 +2,7 @@ import "./commands";
 import "cypress-mailosaur";
 import "cypress-iframe";
 
-// @ts-ignore
-import registerCypressGrep = require("@cypress/grep");
+const registerCypressGrep = require("@cypress/grep");
 registerCypressGrep();
 
 Cypress.on("uncaught:exception", () => {
